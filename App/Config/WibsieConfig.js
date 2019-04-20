ca806681cc2b653804ebe13b83e20c8d3121e252
mm_mac <mike.p.moritz@gmail.com>
2019-04-19 20:33:46 -0400

    update version for staging

--- App/Config/WibsieConfig.js
+++ App/Config/WibsieConfig.js
@@ -1,10 +1,10 @@
 // Wibsie application configuration
 
 export default {
-  endpointAPI: 'https://api.wibsie.com/app',
-  endpointML: 'https://api.wibsie.com/ml',
-  // endpointAPI: 'https://dev-api.wibsie.com/app',
-  // endpointML: 'https://dev-api.wibsie.com/ml',
+  // endpointAPI: 'https://api.wibsie.com/app',
+  // endpointML: 'https://api.wibsie.com/ml',
+  endpointAPI: 'https://dev-api.wibsie.com/app',
+  endpointML: 'https://dev-api.wibsie.com/ml',
   // endpointAPI: 'http://127.0.0.1/app',
   // endpointML: 'http://127.0.0.1/ml',
   authToken: '6d2a3a86ae6b4fffa5448c6bcb5c6c34',
